91d445a0a8d3ad3e508524f4bb6a1e04b927c34a
felipeberrio <cristianberrio95@gmail.com>
2023-03-15 10:35:25 -0500

    style and new function

--- src/main.js
+++ src/main.js
@@ -1,22 +1,38 @@
-const getGatos = () => {
-    cargarFotosGato();
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=4';
+
+
+
+
+const app = () => {
+    loadRandomMichis();
 };
 
-async function cargarFotosGato() {
+async function loadRandomMichis() {
     try{
-        const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
         const response = await fetch(API_URL);
         const data = await response.json();
         console.log(data);
         const img1 = document.getElementById('foto1');
         const img2 = document.getElementById('foto2');
         const img3 = document.getElementById('foto3');
+        const img4 = document.getElementById('foto4');
         img1.src = data[0].url;
         img2.src = data[1].url;
         img3.src = data[2].url;
+        img4.src = data[3].url;
     }catch (error){
         console.log(error);
     }
 }
 
-getGatos();
+async function favoritesMichis(){
+    try{
+        
+
+    }catch{
+
+    }
+}
+
+
+app();
