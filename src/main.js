a0bf76df7d4cf99da1dc863a55dd5ef0e24915c4
felipeberrio <cristianberrio95@gmail.com>
2023-03-16 05:08:23 -0500

    Actualizando función favoritos

--- src/main.js
+++ src/main.js
@@ -1,36 +1,58 @@
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=4';
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=4&api_key=17d94b92-754f-46eb-99a0-65be65b5d18f';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=3';
 
-
-
+const SPAM_ERROR = document.getElementById('error');
 
 const app = () => {
     loadRandomMichis();
+    loadFavoriteMichis();
 };
 
 async function loadRandomMichis() {
     try{
-        const response = await fetch(API_URL);
+        const response = await fetch(API_URL_RANDOM);
         const data = await response.json();
+        console.log('RANDOM');
         console.log(data);
+
         const img1 = document.getElementById('foto1');
         const img2 = document.getElementById('foto2');
         const img3 = document.getElementById('foto3');
-        const img4 = document.getElementById('foto4');
         img1.src = data[0].url;
         img2.src = data[1].url;
         img3.src = data[2].url;
-        img4.src = data[3].url;
+
+        
+        
     }catch (error){
+        SPAM_ERROR.innerHTML = 'Hubo un error: ';
         console.log(error);
     }
 }
 
-async function favoritesMichis(){
+async function loadFavoriteMichis(){
     try{
-        
+        const response = await fetch(API_URL_FAVORITES);
+        const data = await response.json();
+        const img4 = document.getElementById('foto4');
 
-    }catch{
+        if (!response.ok) {
+            throw new Error(`Response error: ${response.status}`);
+        }
 
+        if (data.length > 0) {
+            img4.src = data[0].url;
+        } else {
+            SPAM_ERROR.innerHTML = 'No se encontraron favoritos';
+        }
+
+        console.log('FAVORITES');
+        console.log(data);
+
+    }catch(error){
+        const errorMessage = `Hubo un error: ${error.status} ${error.message}`;
+        SPAM_ERROR.innerHTML = errorMessage;
+        console.log(errorMessage);
     }
 }
 
